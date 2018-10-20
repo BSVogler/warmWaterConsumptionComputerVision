902394142a8c1573ef42f454f9547eb4c2cf7c8b
BSVogler <mail@benediktsvogler.com>
2018-10-20 12:33:52 +0200

    use single folder for images

--- takepicture.py
+++ takepicture.py
@@ -10,16 +10,15 @@ import subprocess
 led = LED(3)
 while(True):
     currentTime = datetime.datetime.now()
-    directory = "/home/pi/"+str(currentTime.strftime('%Y-%m-%d')+"/")
+    directory = "~/warmwaterimages/"
     if not os.path.isdir(directory):
         print("created folder at "+ directory)
         os.makedirs(directory)
     led.on()
     #take picture
-    filename = directory + currentTime.strftime('%H_%-M')+".jpg"
+    filename = directory + str(currentTime.strftime('%y-%m-%d-%H-%M'))+".jpg"
     subprocess.call("raspistill -n -w 1280 -h 720 -o "+filename+" -v -a 12", shell=True)
     print("took picture "+filename)
     led.off()
     #time.sleep(10)#debug
-    time.sleep(30*60)#30 min
-    
+    time.sleep(30*60) # 30 min
